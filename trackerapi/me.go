b61684122c694e8755c402bf4c3cb09587922b9d
Yuriy Hanysh <yuriyhanysh@Yuriys-MacBook-Pro.local>
2018-07-10 15:55:34 +0300

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/yuriyhanysh/clirescue/cmdutil"
+	"github.com/yuriyhanysh/clirescue/user"
 )
 
 var (
