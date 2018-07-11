342475adaa9b7d9d67675c4705691a8ba1384306
Pavlo Hlushchenko <pavlo.hlushchenko@gmail.com>
2018-07-11 17:05:19 +0300

    Remove unused fields from meResponse struct

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -94,11 +94,4 @@ type meResponse struct {
 	APIToken string `json:"api_token"`
 	Username string `json:"username"`
 	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Initials string `json:"initials"`
-	Timezone struct {
-		Kind      string `json:"kind"`
-		Offset    string `json:"offset"`
-		OlsonName string `json:"olson_name"`
-	} `json:"time_zone"`
 }
